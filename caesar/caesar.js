f062d4752a1b4512efa2b8ebd11878596b669783
Adeleke Temitayo <lekedevrepo@gmail.com>
2020-02-26 11:37:31 -0600

    write test to handle punctuation in a message

---
+++ caesar/caesar.js
@@ -0,0 +1,16 @@
+const caesar = (text, key) => text.split('').map(ch => shift(ch, key)).join('');
+
+const shiftMod = (n, m) => (n % m + m) % m;
+
+const charGroup = (char) => (char < 90 ? 65 : 97);
+
+const shift = (char, key) => {
+  const charCode = char.charCodeAt();
+
+  if (charCode <= 90 && charCode >= 65 || charCode <= 122 && charCode >= 97) {
+    return String.fromCharCode(shiftMod(charCode + key - charGroup(charCode), 26) + charGroup(charCode));
+  }
+  return char;
+};
+console.log(caesar('caesar cipher, and his crypto Algorithms!', 18));
+module.exports = caesar;
--- caesar/caesar.test.js
+++ caesar/caesar.test.js
@@ -1,5 +1,15 @@
-const caesar = require('caesar');
+const caesar = require('./caesar');
 
-test('encrypt hello world', () => {
-  expect(caesar('ABC', 4)).toBe('DEF');
+test('test caesar cipher with normal strings', () => {
+  expect(caesar('Hello world', 4)).toBe('Lipps asvph');
+  expect(caesar('ABC', 4)).toBe('EFG');
+});
+
+test('decrypt caesar cipher', () => {
+  expect(caesar('Lipps asvph', -4)).toBe('Hello world');
+  expect(caesar('EFG', -4)).toBe('ABC');
+});
+
+test('test caesar cipher with exclamation and punctuation', () => {
+  expect(caesar('caesar cipher, and his crypto Algorithms!', 18)).toBe('uswksj uahzwj, sfv zak ujqhlg Sdygjalzek!');
 });
